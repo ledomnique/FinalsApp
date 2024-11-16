133fa986055684dbb7c3d417570fa252bac9aee1
Ed Perez <perezedrienne.23@gmail.com>
2024-11-17 01:51:56 +0800

    login page update | login css | login page layout added

--- FinalsApp/Controllers/HomeController.cs
+++ FinalsApp/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace FinalsApp.Controllers
         {
             return View();
         }
+
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
     }
 }
