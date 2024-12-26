ff3b1a2631a734a80aa1a942f4593a3bc91c57ae
Lewis <126696436+ledomnique@users.noreply.github.com>
2024-12-27 02:08:31 +0800

    Update RouteConfig.cs

--- FinalsApp/App_Start/RouteConfig.cs
+++ FinalsApp/App_Start/RouteConfig.cs
@@ -16,7 +16,6 @@ namespace FinalsApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                 defaults: new { controller = "Home", action = "AdminDashboard", id = UrlParameter.Optional }
 
             );
